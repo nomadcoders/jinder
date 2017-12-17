892b4ae4a0ce09fd8c367f1f90858a7eb23a939f
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-12-17 13:47:11 +0700

    JWT plugin

---
+++ config.js
@@ -0,0 +1,1 @@
+const key = "KVcZMXlErXeyiT7pQlrMB3VzGHzOMaOz";
--- server.js
+++ server.js
@@ -1,17 +1,44 @@
 const Hapi = require("hapi");
-const db = require("./db");
+const secret = require("./config");
+const glob = require("glob");
+const jwt = require("hapi-auth-jwt");
 
+// Import the DB connection
+const db = require("./db").db;
+
+// Create the server
 const server = new Hapi.Server({ port: 3000, host: "localhost" });
 
+// Register the JWT
+
+const registerJWT = async () => {
+  try {
+    await server.register({ plugin: require("hapi-auth-jwt") });
+    server.auth.strategy("jwt", "jwt", {
+      key: secret,
+      verifyOptions: { algorithms: ["HS256"] }
+    });
+    glob.sync("api/**/routes/*.js", { root: __dirname }).forEach(file => {
+      const route = require(path.join(__dirname, file));
+      server.route(route);
+    });
+  } catch (error) {
+    console.log(error, "❌");
+  }
+};
+
+// Start function
 const start = async () => {
   try {
     await server.start();
   } catch (error) {
-    console.log(error);
+    console.log(error, "❌");
     process.exit(1);
   }
 
   console.log("Server running at:", server.info.uri, "✅");
 };
 
+// Start the server
 start();
+registerJWT();
